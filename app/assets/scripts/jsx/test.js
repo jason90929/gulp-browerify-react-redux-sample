fd832de013b6f576526c1f723ccd05b50ff5ee6b
Jason Tseng <tsaistorm@gmail.com>
2016-08-11 16:32:34 +0800

    First Commit

---
+++ app/assets/scripts/jsx/test.js
@@ -0,0 +1,9 @@
+module.exports = function() {
+    console.log('hello');
+
+    // ReactDOM.render(
+    //     <h1>Hello, world!</h1>,
+    //     document.getElementById('example')
+    // );
+
+};
---
+++ app/assets/scripts/main.js
@@ -0,0 +1,6 @@
+var react = require('react');
+var redux = require('redux');
+
+// var test = require('./jsx/test.js');
+// test();
+
